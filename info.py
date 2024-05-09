7d596628f5ff41b175f021a19cc588bf44dd0ab9
Md Matin Ashraf <91468485+Ashrafmdmatin41@users.noreply.github.com>
2024-05-09 09:00:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -28,7 +28,7 @@ SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '20551282'))
 API_HASH = environ.get('API_HASH', 'f2aa48726d861f983ddb5f8f8d044a1e')
 OWNER_ID = environ.get('OWNER_ID', '6606050198')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7099618649:AAHXbv6FQj5GBLfVwbPGwkGZogdHaBvzGhk")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7018018136:AAFJ3xHuNBRmSXGRm4_0_M1HLvvLYFYMTDA")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -39,7 +39,7 @@ NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/549fd9f3272214acade82.j
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/ccf3cbc4687263ac63420.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/549fd9f3272214acade82.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/347c1f79f36d3cf14e0f5.jpg'))
-CODE = (environ.get('CODE', 'https://graph.org/file/02e7ecc3e2693b481b914.jpg'))
+CODE = (environ.get('CODE', 'https://telegra.ph/file/def284d991edabc59600a.jpg'))
 
 
 # Command
@@ -52,7 +52,7 @@ EVAL_ID = environ.get("EVAL_ID", "-1002035521908")
 # Referal Settings
 REFERAL_COUNT = int(environ.get('REFERAL_COUNT', '20')) # number of referal count
 REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1 week')
-OWNER_USERNAME = environ.get('OWNER_USERNAME', 'sewxiy') # owner username without @
+OWNER_USERNAME = environ.get('OWNER_USERNAME', 'MatizTechzOwner') # owner username without @
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6606050198').split()]
@@ -60,7 +60,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '6606050198').split()]
-auth_channel = environ.get('AUTH_CHANNEL', '') #Channel / Group Id for force sub ( make sure bot is admin )
+auth_channel = environ.get('AUTH_CHANNEL', '-1002046725997) #Channel / Group Id for force sub ( make sure bot is admin )
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
@@ -89,8 +89,8 @@ TEXT = environ.get("APPROVED_WELCOME_TEXT", "<b>{mention},\n\nʏᴏᴜʀ ʀᴇǫ
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'shareus.io'))
-STREAM_API = (environ.get('STREAM_API', ''))
+STREAM_SITE = (environ.get('STREAM_SITE', 'publicearn.com'))
+STREAM_API = (environ.get('STREAM_API', '2b4e167841be8af6f1d3254f2fccd2bed5518b19'))
 STREAMHTO = (environ.get('STREAMHTO', 't.me/Matiz_Techz'))
 
 # Verify
@@ -98,16 +98,16 @@ VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( F
 HOWTOVERIFY = environ.get('HOWTOVERIFY', 't.me/Matiz_Techz') # How to open tutorial link for verification
 
 # Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'publicearn.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', '2b4e167841be8af6f1d3254f2fccd2bed5518b19')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-BOT_USERNAME = environ.get("BOT_USERNAME", "TxtToFileDLNewBot")
+BOT_USERNAME = environ.get("BOT_USERNAME", "ForTestTwoBot")
 BOT_NAME = environ.get("BOT_NAME", "𝐋ᴜᴄʏ")
-BOT_ID = environ.get("BOT_ID", "6040310745")
+BOT_ID = environ.get("BOT_ID", "7018018136")
 S_GROUP = environ.get('S_GROUP', "scited")
 S_CHANNEL = environ.get('S_CHANNEL', "Three_Body_Problem_Hindi")
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie7xchat')
