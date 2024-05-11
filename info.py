2ebd1451cc0ef03afc5c9e22e7aaeb0e56170d4a
Md Matin Ashraf <91468485+Ashrafmdmatin41@users.noreply.github.com>
2024-05-11 10:50:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ REFERAL_PREMEIUM_TIME = environ.get('REFERAL_PREMEIUM_TIME', '1 week')
 OWNER_USERNAME = environ.get('OWNER_USERNAME', 'LuciG0d') # owner username without @
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1966983383 6051730362').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1966983383 5279550771 6500956112').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
